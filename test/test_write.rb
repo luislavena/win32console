653ed81f92793440be61a04325c4c75ec01b2653
Luis Lavena <luislavena@gmail.com>
2010-03-07 15:26:27 +0100

    Abort tests when term-ansicolor is missing.

--- test/test_write.rb
+++ test/test_write.rb
@@ -6,6 +6,7 @@ begin
 rescue LoadError
   puts "You need term-ansicolor gem installed to run this test"
   puts "Please 'gem install term-ansicolor' and try again."
+  exit(1)
 end
 
 require "benchmark"
