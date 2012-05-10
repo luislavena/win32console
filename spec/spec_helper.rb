011fa4e556abbe14ac67859af8829a978a9be01f
Luis Lavena <luislavena@gmail.com>
2012-05-10 13:59:57 -0300

    No need to play with the LOAD_PATH
    Both `rake spec` and `rspec spec` will add 'lib' to the $LOAD_PATH

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,3 @@
-# Add the projects lib directory to our load path so we can require libraries
-# within it easily.
-dir = File.expand_path(File.dirname(__FILE__))
-
-SPECDIR = dir
-$LOAD_PATH.unshift("#{dir}/../lib")
-
 require 'rubygems'
 require 'mocha'
 require 'rspec'
